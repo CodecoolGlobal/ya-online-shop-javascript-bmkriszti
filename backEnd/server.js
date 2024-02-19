430d8b7359f5add3a6faae366af7b711de50628f
BalintCoder <gbalint2000@gmail.com>
2024-02-19 14:48:11 +0100

    files are done

---
+++ backEnd/server.js
@@ -0,0 +1,38 @@
+import { isUtf8 } from "buffer";
+import express, { json } from "express"
+import fs from "fs/promises";
+import path, { parse } from "path";
+import url from "url";
+
+const filename = url.fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+
+const app = express ()
+app.use(express.json());
+
+app.use(express.static(path.join(dirname, "../frontEnd")));
+
+
+
+app.get("/api/bath", async (req, res) => {
+    try {
+        const data = await fs.readFile("./data.json", "utf-8")
+    const parseData = JSON.parse(data)
+    res.send(parseData)
+        
+    } catch (error) {
+        console.log(error + "Error, cannot get api/bath")
+    }
+})
+
+
+
+
+
+
+
+
+app.listen(8080, () => {
+    console.log("Server running on http://localhost:8080");
+});
