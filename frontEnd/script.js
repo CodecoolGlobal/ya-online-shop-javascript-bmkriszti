a1f1631078d7069e1095fa19be3a7fd8deeda92d
BalintCoder <gbalint2000@gmail.com>
2024-02-19 15:27:48 +0100

    displaying data in editor site

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -2,43 +2,41 @@ const fetchData = async (url) => {
     try {
         const response = await fetch(url);
         if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`)
+            throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        const data = await response.json()
-        console.log(data)
-        return data
-
+        const data = await response.json();
+        console.log(data);
+        return data;
     } catch (error) {
-        console.error(`Error Fetching data: ${error.message}`);
-        throw error
-    }
-}
-
-const main = async () => {
-    const response = await fetch("/api/bath")
-    const bombs = await response.json()
-    const rootElement = document.getElementById("root")
-    try {
-        rootElement.insertAdjacentHTML("beforeend", bathBombHTML(bombs))
-    } catch (error) {
-        console.error(`Error in main: ${error.message}`);
+        console.error(`Error fetching data: ${error.message}`);
+        throw error;
     }
 };
 
-const bathBombHTML = async () => {
+const renderBathBombHTML = async () => {
     try {
         const bombs = await fetchData("/api/bath");
         const liElements = bombs.map(bomb => `
                 <li>
                     <span>${bomb.name}</span>
-                    <button class="edit" data-bombid="bomb">Edit</button>
-                    <button class="delete" data-bombid="bomb">Delete</button>
+                    <button class="edit" data-bombid="${bomb.name}">Edit</button>
+                    <button class="delete" data-bombid="${bomb.name}">Delete</button>
                 </li>
             `);
         return `<ul>${liElements.join('')}</ul>`;
     } catch (error) {
         console.error(`Error creating HTML: ${error.message}`);
         throw error;
+    }
+};
+
+const main = async () => {
+    const rootElement = document.getElementById("root");
+    try {
+        const bathBombHTML = await renderBathBombHTML();
+        rootElement.insertAdjacentHTML("beforeend", bathBombHTML);
+    } catch (error) {
+        console.error(`Error in main: ${error.message}`);
     }
 };
 
