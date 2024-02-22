480d31b412bdf790b0c905f2b491cbede21bc4e9
BalintCoder <gbalint2000@gmail.com>
2024-02-22 11:15:42 +0100

    almostdone

--- frontEnd/cart.js
+++ frontEnd/cart.js
@@ -1,18 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     const productsInCart = JSON.parse(localStorage.getItem('productsInCart'));
-    const parsedProductsInCart = productsInCart.map(products => JSON.parse(products))
 
-    console.log(productsInCart);
+    
 
-    const cartHtml = (product)=>{
-     let HTML = ``
-        product.forEach(p=>{
-            HTML+= `
-            <h2>${p.name}</h2>
-            <p>Price: ${p.price}</p>
-        `
-        })
-        return HTML
+    if (productsInCart && productsInCart.length > 0) {
+        const parsedProductsInCart = productsInCart.map(product => JSON.parse(product));
+
+        const cartHtml = (products) => {
+            let HTML = '';
+            let totalPrice = 0;
+
+            products.forEach(product => {
+                HTML += `
+                    <div class='cart' id="${product.id}">
+                        <h2>${product.name}</h2>
+                        <p>Price: ${product.price}Ft</p>
+                    </div>
+                `;
+                totalPrice += parseInt(product.price);
+            });
+
+            HTML += `<h3>Total Price: ${totalPrice}Ft</h3>`;
+            return HTML;
+        }
+
+        document.getElementById('cart-container').insertAdjacentHTML('beforeend', cartHtml(parsedProductsInCart));
+    } else {
+        document.getElementById('cart-container').innerHTML = "<p>No products in cart.</p>";
     }
-    document.getElementById('cart-container').insertAdjacentHTML('beforeend', cartHtml(parsedProductsInCart))
-})
+});
