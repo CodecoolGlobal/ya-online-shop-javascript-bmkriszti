881adc1716d20631bd4f58055dd52fae7ef4bb47
bmkriszti <bmkriszti97@gmail.com>
2024-02-22 11:14:23 +0100

    done

--- frontEnd/cart.js
+++ frontEnd/cart.js
@@ -1,22 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const productsInCart = JSON.parse(localStorage.getItem('productsInCart'));
-    const parsedProductsInCart = productsInCart.map(products => JSON.parse(products))
 
-    console.log(productsInCart);
+    if (productsInCart && productsInCart.length > 0) {
+        const parsedProductsInCart = productsInCart.map(product => JSON.parse(product));
 
     const cartHtml = (product)=>{
      let HTML = ``
+     let totalPrice = 0;
      
         product.forEach(p=>{
             HTML+= `
             <h2>${p.name}</h2>
             <p>Price: ${p.price}</p>
         `
+        totalPrice += parseInt(p.price);
         })
-        return HTML
+        HTML += `<h3>Total Price: ${totalPrice}</h3>`;
+            return HTML;
     }
-    document.getElementById('cart-container').insertAdjacentHTML('beforeend', cartHtml(parsedProductsInCart) + `<button id='checkout'>Check out here</button>`)
+    document.getElementById('cart-container').insertAdjacentHTML('beforeend', cartHtml(parsedProductsInCart)+ `<button id='checkout'>Check out here</button>`);
+    } else {
+        document.getElementById('cart-container').innerHTML = "<p>No products in cart.</p>";
+    }
     document.getElementById('checkout').addEventListener('click', ()=>{
         window.alert('Thank you for visiting our online shop. Please note that this is a test page for demonstration purposes, and no actual purchases have been made. We appreciate your interest in our products and hope you enjoyed browsing our selection. If you have any questions or feedback, feel free to contact us. Thank you for your money though.')
-    } )
+    })
 })
